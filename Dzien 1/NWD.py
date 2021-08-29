bddd3acf80ec07a051292949cc78d1a59d3c6253
Kacper <kapisowskyytv@gmail.com>
2021-08-29 11:24:40 +0200

    Update NWD.py
    [solved]

--- Dzien 1/NWD.py
+++ Dzien 1/NWD.py
@@ -1,1 +1,15 @@
+# zdefiniowanie funkcji obliczającej nwd
 def nwd(a, b): return nwd(b, a % b) if b else a
+
+
+wykonane = []
+testy = int(input())
+
+for i in range(testy):
+    # wczytanie więcej niż jednego argumentu
+    x = list(map(int, input().split()))
+    # dodanie do listy 'wykonane' wyniku funkcji
+    wykonane.append(nwd(x[0], x[1]))
+
+for i in wykonane:  # wypisanie zawartosci funkcji wykonane
+    print(i)
