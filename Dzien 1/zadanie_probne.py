e40e1f8408bb11aae2d9b24b1ad69e3d53fb227e
Kacper <kapisowskyytv@gmail.com>
2021-08-29 14:03:52 +0200

    Update zadanie_probne.py

--- Dzien 1/zadanie_probne.py
+++ Dzien 1/zadanie_probne.py
@@ -1,3 +1,5 @@
-a = input()
-b = input()
-print (int(a)+int(b))
+# program jest bardzo uproszczony ponieważ spoj nie lubi pythona
+# inne opcje np. funkcja dodająca oraz zwarająca wynik - wypada błąd wykonania
+a = input()  # wprowadz a
+b = input()  # wprowadz b
+print(int(a)+int(b))  # wypisz wynik dodawania a i b
